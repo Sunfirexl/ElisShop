8d840e704fae21fe06d92f6afd3ec608b9250ee5
SunfireDev <sunfiredeveloping@gmail.com>
2018-10-26 09:56:45 -0500

    Initial Solution Setup

---
+++ ElisShop/ElisShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElisShop.WebUI.Startup))]
+namespace ElisShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
